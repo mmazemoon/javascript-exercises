d949a12a2e72e3a514bc9f977e386c1d833b92df
Mark Azemoon <mmazemoon@gmail.com>
2015-05-22 08:30:00 -0700

    Add twoSum method that finds all pairs of positions where the elements sum to zero

--- arrays.js
+++ arrays.js
@@ -2,14 +2,31 @@
 // my uniq
 
 Array.prototype.myUniq = function () {
-  result = [];
+  var result = [];
   this.forEach(function (el) {
     if (result.indexOf(el) === -1) {
       result.push(el);
     }
-  })
+  });
   console.log(result);
   return result;
 };
 
-[1, 2, 1, 3, 3].myUniq();
+// [1, 2, 1, 3, 3].myUniq();
+
+// twoSum
+
+Array.prototype.twoSum = function () {
+  var result = [];
+  for(var i = 0; i < this.length - 1; i++) {
+    for(var j = i + 1; j < this.length; j++) {
+        if (this[i] + this[j] === 0) {
+          result.push([i,j]);
+        }
+    }
+  }
+  console.log(result);
+  return result;
+};
+
+// [-1, 0, 2, -2, 1].twoSum();
