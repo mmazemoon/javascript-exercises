ca11dd895af8eb4a5978faeaf01573b1ba72f55a
Mark Azemoon <mmazemoon@gmail.com>
2015-05-22 07:35:03 -0700

    Implelemt myUniq function which removes duplicate elements from an array.

--- arrays.js
+++ arrays.js
@@ -1,3 +1,15 @@
+
 // my uniq
 
-Array.prototype.myUniq = function () {};
+Array.prototype.myUniq = function () {
+  result = [];
+  this.forEach(function (el) {
+    if (result.indexOf(el) === -1) {
+      result.push(el);
+    }
+  })
+  console.log(result);
+  return result;
+};
+
+[1, 2, 1, 3, 3].myUniq();
