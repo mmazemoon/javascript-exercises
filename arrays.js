bccb2df6c63b3b6d3524f3e0fb1cd2dde0637f3a
Mark Azemoon <mmazemoon@gmail.com>
2015-05-22 18:49:09 -0700

    Implement myTranspose which converts between the row-oriented and column-oriented representations.

--- arrays.js
+++ arrays.js
@@ -30,3 +30,25 @@ Array.prototype.twoSum = function () {
 };
 
 // [-1, 0, 2, -2, 1].twoSum();
+
+Array.prototype.myTranspose = function () {
+  var result = [];
+  for (var i = 0; i < this[0].length; i++) {
+      result[i] = [];
+  }
+  for(var i = 0; i < this.length; i++) {
+    for(var j = 0; j < this[i].length; j++) {
+      result[j][i] = this[i][j];
+    }
+  }
+  console.log(result);
+  return result;
+};
+
+
+[ [1, 2],
+  [3, 4],
+  [5, 6 ]].myTranspose();
+
+[ [1, 3, 5 ],
+  [2, 4, 6]].myTranspose();
