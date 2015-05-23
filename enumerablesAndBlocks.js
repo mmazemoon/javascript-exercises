0b173005dc6eaaf77559a23b3c30536592aca558
Mark Azemoon <mmazemoon@gmail.com>
2015-05-23 09:57:15 -0700

    Add myMap which uses myEach and a closure to return a new array with the results of the mapped elements

--- enumerablesAndBlocks.js
+++ enumerablesAndBlocks.js
@@ -3,4 +3,18 @@ Array.prototype.myEach = function (callback) {
   for(var i = 0; i < this.length; i++){
     callback(this[i]);
   }
+  return this;
 };
+
+// myMap
+Array.prototype.myMap = function (callback){
+  var result = [];
+  this.myEach (function (el) {
+    result.push(callback(el));
+  });
+  console.log(result);
+  return result;
+};
+
+var integers = [1, 2, 3, 4];
+integers.myMap(function(i){ return i*i; }); // => [1, 4, 9, 16]
