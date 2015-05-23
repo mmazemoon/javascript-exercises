541973999186166d8added2671ad4440fa6c5bb3
Mark Azemoon <mmazemoon@gmail.com>
2015-05-23 11:00:10 -0700

    Implement myInject which takes in a function and accumulates the values of elements in the collection.

--- enumerablesAndBlocks.js
+++ enumerablesAndBlocks.js
@@ -18,3 +18,19 @@ Array.prototype.myMap = function (callback){
 
 var integers = [1, 2, 3, 4];
 integers.myMap(function(i){ return i*i; }); // => [1, 4, 9, 16]
+
+// myInject
+Array.prototype.myInject = function (callback) {
+  var memo = this[0];
+  this.slice(1).myEach(function(el){
+    memo = callback(memo, el);
+  });
+  console.log(memo);
+  return memo;
+};
+
+[1,2,3,4,5].myInject(function(memo, el){
+  return memo + el;
+});
+
+// A function returns undefined if a value was not returned.
